1d5d2765ea326e44dfe16020cccbad76eaf00b1f
coderkunal <shubham123>
2021-11-20 11:07:21 +0530

    add tryctach block

--- app/src/main/java/com/status/kotlinrecyclerview/MainActivity.kt
+++ app/src/main/java/com/status/kotlinrecyclerview/MainActivity.kt
@@ -22,26 +22,32 @@ private lateinit var exampleAdapter: ExampleAdapter
     private fun initializeViews()
     {
 
-        recyclerView=findViewById(R.id.recyclerview)
+        try{
 
-        val list=ArrayList<ExampleItem>()
+            recyclerView=findViewById(R.id.recyclerview)
 
-        list.add(ExampleItem(R.drawable.ic_android_black_24dp, "shubham","karan"))
-        list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","ss"))
-        list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","dd"))
-        list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","gg"))
-        list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","hh"))
-        list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","jk"))
+            val list=ArrayList<ExampleItem>()
 
-        val linearLayoutManager=LinearLayoutManager(this)
-        linearLayoutManager.orientation=LinearLayoutManager.VERTICAL
-        exampleAdapter= ExampleAdapter(this,list)
-        recyclerView.apply {
+            list.add(ExampleItem(R.drawable.ic_android_black_24dp, "shubham","karan"))
+            list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","ss"))
+            list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","dd"))
+            list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","gg"))
+            list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","hh"))
+            list.add(ExampleItem(R.drawable.ic_android_black_24dp, "ff","jk"))
 
-            setHasFixedSize(true)
-            adapter=exampleAdapter
-            layoutManager=linearLayoutManager
+            val linearLayoutManager=LinearLayoutManager(this)
+            linearLayoutManager.orientation=LinearLayoutManager.VERTICAL
+            exampleAdapter= ExampleAdapter(this,list)
+            recyclerView.apply {
 
+                setHasFixedSize(true)
+                adapter=exampleAdapter
+                layoutManager=linearLayoutManager
+
+            }
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
         }
     }
 }
